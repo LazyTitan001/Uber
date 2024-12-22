95935550d5e425d13ee2540f7396d87638532fbd
LazyTitan001 <rsr182003@gmail.com>
2024-12-23 01:06:58 +0530

    add ride confirmation feature; implement confirmRide method and update ride status

--- backend/controllers/rides.controller.js
+++ backend/controllers/rides.controller.js
@@ -2,6 +2,7 @@ const rideService = require('../services/ride.service');
 const { validationResult } = require('express-validator');
 const mapService = require('../services/maps.service');
 const { sendMessageToSocketId } = require('../socket');
+const rideModel = require('../models/ride.model');
 
 
 module.exports.createRide = async (req, res) => {
@@ -12,12 +13,16 @@ module.exports.createRide = async (req, res) => {
         console.log(pickupCoordinates);
         const captainsInRadius = await mapService.getCaptainsInTheRadius(pickupCoordinates.ltd, pickupCoordinates.lng, 2);
         ride.otp = "";
+        const rideWithUser = await rideModel.findOne({ _id: ride._id }).populate('user');
+
         captainsInRadius.map(captain => {
+
             sendMessageToSocketId(captain.socketId, {
                 event: 'new-ride',
-                data: ride
-            });
-        });
+                data: rideWithUser
+            })
+
+        })
         return res.status(201).json(ride);
     } catch (err) {
         return res.status(500).json({ message: err.message });
@@ -36,6 +41,30 @@ module.exports.getFare = async (req, res) => {
         const fare = await rideService.getFare(pickup, destination);
         return res.status(200).json(fare);
     } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports.confirmRide = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { rideId } = req.body;
+
+    try {
+        const ride = await rideService.confirmRide({ rideId, captain: req.captain });
+
+        sendMessageToSocketId(ride.user.socketId, {
+            event: 'ride-confirmed',
+            data: ride
+        })
+
+        return res.status(200).json(ride);
+    } catch (err) {
+
+        console.log(err);
         return res.status(500).json({ message: err.message });
     }
 }
--- backend/routes/rides.routes.js
+++ backend/routes/rides.routes.js
@@ -6,6 +6,8 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/create', authMiddleware.authUser, rideController.createRide);
 router.get('/get-fare', authMiddleware.authUser, rideController.getFare);
+router.post('/confirm',authMiddleware.authCaptain,rideController.confirmRide);
+
 
 
 module.exports = router;
