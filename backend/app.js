7ab88283ce194171dd5450195f3600efdd5a6cca
LazyTitan001 <rsr182003@gmail.com>
2024-12-26 13:33:22 +0530

    add CORS middleware; enable cross-origin requests with appropriate headers

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,15 @@ const { initializeSocket } = require('./socket');
 const http = require('http');
 
 const app = express();
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
